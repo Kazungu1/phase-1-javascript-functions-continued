e7fa432f9b4c9a0bfdafed459e29da3eba2cfe1b
Kazungu1 <kazunguderriq@gmail.com>
2022-06-26 23:05:35 +0300

    add last function

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ const mondayWork = function(activity="go to the office"){
     return `This Monday, I will ${activity}.`;
 }
 mondayWork("Go for a hike");
+
+(function (baseNumber){
+    return baseNumber+3;
+})(2);
+
+
+function outer(greeting, msg="its a fine day to learn"){
+    const innerFunction = function(name,lang="Python"){
+        return `${greeting}, ${name}! ${msg} ${lang}`;
+
+    };
+    return innerFunction("student", "javascript");
+}
+outer("Hello");
+function wrapAdjective(style) {
+    let result =(function(activity) {
+        return "You are " +style+ activity+
+        style + "!"
+    })
+    let emphatic = result;
+    return emphatic;   
+}
